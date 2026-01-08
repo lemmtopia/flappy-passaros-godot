6746b0760137ab7c9c5e203b1b91e823c4e48be1
Gabriel G. Cunha <gabrielmesmo97@gmail.com>
2026-01-08 11:22:28 -0300

    Add gameover

--- flappy-passaros/scripts/bird.gd
+++ flappy-passaros/scripts/bird.gd
@@ -9,3 +9,10 @@ func _physics_process(delta: float) -> void:
 		velocity.y = flap_speed
 	
 	move_and_slide()
+
+func game_over():
+	get_tree().call_deferred("reload_current_scene")
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("trees"):
+		game_over()
--- flappy-passaros/scripts/tree.gd
+++ flappy-passaros/scripts/tree.gd
@@ -1,4 +1,15 @@
 extends Node2D
 
-func _on_area_2d_area_entered(area: Area2D) -> void:
-	pass # Replace with function body.
+signal bird_detected
+
+@export var speed = 200
+
+func _process(delta: float) -> void:
+	position.x -= speed * delta
+	
+	if position.x < -141:
+		queue_free()
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.name == "Bird":
+		emit_signal("bird_detected")
