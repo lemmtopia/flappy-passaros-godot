1c845c94699b81b16dcf71809e41b111653ddc23
Gabriel G. Cunha <137636549+lemmtopia@users.noreply.github.com>
2026-01-12 17:05:11 -0300

    Start menu anim

--- flappy-passaros/scripts/start_menu_layer.gd
+++ flappy-passaros/scripts/start_menu_layer.gd
@@ -3,8 +3,10 @@ extends CanvasLayer
 signal start_game
 
 @onready var start_button: Button = $StartButton
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _on_start_button_pressed() -> void:
 	start_button.disabled = true
 	visible = false
+	animation_player.stop()
 	start_game.emit()
