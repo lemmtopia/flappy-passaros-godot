9ad8b559ccf963d1a60610e7ffb248847f466386
Gabriel G. Cunha <137636549+lemmtopia@users.noreply.github.com>
2026-01-12 09:27:25 -0300

    Better gameover

--- flappy-passaros/scripts/bird.gd
+++ flappy-passaros/scripts/bird.gd
@@ -1,6 +1,10 @@
 extends CharacterBody2D
+class_name Player
 
 signal hit
+
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 @export var gravity : float = 500
 @export var flap_speed : float = -200
@@ -17,7 +21,12 @@ func _physics_process(delta: float) -> void:
 func game_over():
 	can_control = false
 	$RestartTimer.start()
+	call_deferred("defeated_behaviour")
 	emit_signal("hit")
+
+func defeated_behaviour():
+	collision_shape_2d.disabled = true
+	animated_sprite_2d.stop()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("trees"):
--- flappy-passaros/scripts/enemy.gd
+++ flappy-passaros/scripts/enemy.gd
@@ -2,10 +2,20 @@ extends Node2D
 
 @export var speed : float = 270
 
+var bird : CharacterBody2D
+
 func _ready() -> void:
-	pass # Replace with function body.
+	bird = get_parent().get_node("Bird")
+	bird.connect("hit", stop_moving)
 
 func _process(delta: float) -> void:
 	position.x -= speed * delta
 	if position.x < -70:
 		queue_free()
+
+func stop_moving():
+	speed = 0
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is Player:
+		body.game_over()
